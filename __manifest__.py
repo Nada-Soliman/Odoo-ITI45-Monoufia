70d5fc8ab360c610a50ac809ced57125d0652ff9
Nada-Soliman <nada.mohammad7654@gmail.com>
2025-04-04 12:06:50 +0200

    Day1

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'ITI3',
+    'summary':'iti 3',
+    'author':'Nada-Soliman',
+    'category':'Accounting',
+    'version':'0.1',
+    'depends': ['account','base'],
+    'data':[
+        'views/base_menu.xml',
+        'views/iti_student_views.xml'
+    ]
+}
