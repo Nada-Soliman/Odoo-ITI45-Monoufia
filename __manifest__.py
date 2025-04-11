fc80a1bb0c18c53847ede8b53e8f5e08cd7d3c3d
Nada-Soliman <nada.mohammad7654@gmail.com>
2025-04-11 18:42:12 +0200

    Day4

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,10 @@
     'version':'0.1',
     'depends': ['account','base','product'],
     'data':[
+        'security/ir.model.access.csv',
+        'security/groups.xml',
+        'reports/iti_student_template.xml',
+        'reports/reports.xml',
         'views/base_menu.xml',
         'views/iti_student_views.xml',
         'views/iti_track_views.xml',
